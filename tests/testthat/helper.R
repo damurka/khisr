bba942c1bfd92d2a59ad9c8a4ff81701bd4fc357
Damurka <>
2024-02-03 17:42:14 +0300

    Added to test: spell checking and authentication

---
+++ tests/testthat/helper.R
@@ -0,0 +1,14 @@
+auth_sucess <- tryCatch(
+  khisr:::khis_cred_testing(),
+  khis_cred_internal_error = function(e) NULL
+)
+if(!isTRUE(auth_sucess)) {
+  khisr:::khis_info(c(
+    "!" = "Internal auth failed; calling {.fun khis_cred_clear}."
+  ))
+  khis_cred_clear()
+}
+
+skip_if_no_cred <- function() {
+  testthat::skip_if_not(khis_has_cred(), "No KHIS credentials")
+}
